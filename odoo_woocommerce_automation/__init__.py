f5cdcb55dd8fe39e1d3cbc6717967384b40a7164
Mario Osvaldo Nuñez <marionumza@gmail.com>
2025-10-09 21:27:10 -0300

    Delete odoo_woocommerce_automation directory

--- odoo_woocommerce_automation/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# See LICENSE file for full copyright and licensing details.
-
-from . import models
-from . import wizard
-from . import controllers
-from .tools import post_init_hook, uninstall_hook 
