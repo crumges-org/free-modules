519655dc1af23061dd724369d6c81f4e4aa7a3c2
Mario Nuñez <marionumza@gmail.com>
2026-09-18 21:08:47 -0300

    upd

---
+++ muk_mcp/tools/common.py
@@ -0,0 +1,25 @@
+import json
+
+MCP_SERVER_NAME = 'odoo-mcp-server'
+MCP_SERVER_VERSION = '1.0.0'
+
+JSONRPC_VERSION = '2.0'
+
+JSONRPC_PARSE_ERROR = -32700
+JSONRPC_INVALID_REQUEST = -32600
+JSONRPC_METHOD_NOT_FOUND = -32601
+JSONRPC_INVALID_PARAMS = -32602
+JSONRPC_INTERNAL_ERROR = -32603
+
+MCP_HEADER_MISMATCH = -32020
+
+MCP_UNSUPPORTED_PROTOCOL_VERSION = -32022
+
+
+def coerce_json_value(value):
+    if isinstance(value, str):
+        try:
+            return json.loads(value)
+        except (TypeError, ValueError):
+            return value
+    return value
